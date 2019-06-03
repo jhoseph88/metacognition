e5592eac096b54a40b1d27414695b50ddbf6889c
Cameron Barnes <ccbarnes88@gmail.com>
2019-06-02 20:14:03 -0400

    fix spotify link

--- src/Constants.js
+++ src/Constants.js
@@ -1,6 +1,6 @@
 // Links
 export const ARCHIVE_URL = 'https://s3.amazonaws.com/metacognition/archive.json'
-export const SPOTIFY = 'https://open.spotify.com/show/0T2wtCEYRmeMd6hMCeyXJ8'
+export const SPOTIFY = 'https://open.spotify.com/show/73Di3ZnphRKSFjYpwVYmxf'
 export const ITUNES = 'https://itunes.apple.com/us/podcast/metacognition/id1450133749?mt=2'
 export const RSS = 'https://pcr.apple.com/id1450133749'
 export const RSS_ICON = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Rss-feed.svg/600px-Rss-feed.svg.png'
